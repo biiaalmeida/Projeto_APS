e834c570fecef5cc67e1804418fbe01d205bab46
hillaryds <hillary.saldanha199@gmail.com>
2026-06-17 09:33:32 -0300

    feat: add NivelAcesso and TipoRelatorio enums

---
+++ unidade_III/perfumaria/src/main/java/com/aromas/perfumaria/enums/NivelAcesso.java
@@ -0,0 +1,7 @@
+package com.aromas.perfumaria.enums;
+
+public enum NivelAcesso {
+    GERENTE,
+    VENDEDOR,
+    ADMINISTRADOR
+}
---
+++ unidade_III/perfumaria/src/main/java/com/aromas/perfumaria/enums/TipoRelatorio.java
@@ -0,0 +1,7 @@
+package com.aromas.perfumaria.enums;
+
+public enum TipoRelatorio {
+    VENDAS,
+    PRODUTOS,
+    PROMOCOES
+}
